7bcb5dbe5cae531903ea95f2210a497aa51957bc
Rudolph <azazil47@gmail.com>
2022-09-05 14:40:56 +0700

    Сверстана 5 секция

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -5,13 +5,50 @@
 		slidesPerGroup: 3,
 		spaceBetween: 13,
 		// If we need pagination
-		pagination: {
-			el: '.swiper-pagination',
-		},
+		// pagination: {
+		// 	el: '.swiper-pagination',
+		// 	clickable: true,
+		// },
 		// Navigation arrows
-		navigation: {
-			nextEl: '.swiper-button-next-unique',
-			prevEl: '.swiper-button-prev-unique',
+		// navigation: {
+		// 	enable: true,
+		// 	nextEl: '.swiper-button-next-unique',
+		// 	prevEl: '.swiper-button-prev-unique',
+			
+		// },
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				centeredSlides: true,
+				slidesPerGroup: 1,
+				scrollbar: {
+					el: '.swiper-scrollbar',
+					dragSize: '65px',
+					draggable: true,
+					enabled: true,
+				},
+				
+			},
+			768: {
+				slidesPerView: 2,
+				scrollbar: {
+					el: '.swiper-scrollbar',
+					dragSize: '142px',
+					draggable: true,
+					enabled: true,
+				}
+			},
+			992: {
+				slidesPerView: 3,
+				scrollbar: {
+					enabled: false,
+				},
+				pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+				enabled: true,
+				},
+			},
 		},
 	});
 
