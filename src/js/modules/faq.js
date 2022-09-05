0bcbae7a688ccfe4b30a9d7449e7de1bd55f3973
Rudolph <azazil47@gmail.com>
2022-09-05 17:36:50 +0700

    ДОабвлено разворачивание списка

--- src/js/modules/faq.js
+++ src/js/modules/faq.js
@@ -1,7 +1,8 @@
 export function swipe () {
 	const btn = document.querySelector('.btn_faq');
 	const content = document.querySelector('.content__text');
+	content.style.maxHeight = '450px';
 	btn.addEventListener('click', () => {
-
+		content.style.maxHeight = content.scrollHeight + 'px';
 	})
 }
