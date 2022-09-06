ad256dfaa2059cd08daff666cc1aa4f0c8c78ea2
Rudolph <azazil47@gmail.com>
2022-09-06 10:42:03 +0700

    Сверстана 6 секция

--- src/js/modules/faq.js
+++ src/js/modules/faq.js
@@ -1,8 +1,16 @@
 export function swipe () {
 	const btn = document.querySelector('.btn_faq');
+	const btn_off = document.querySelector('.btn_faq-off');
 	const content = document.querySelector('.content__text');
 	content.style.maxHeight = '450px';
 	btn.addEventListener('click', () => {
 		content.style.maxHeight = content.scrollHeight + 'px';
+		btn.classList.add('press');
+		btn_off.classList.add('active');
+	})
+	btn_off.addEventListener('click', () => {
+		content.style.maxHeight = '450px';
+		btn_off.classList.remove('active');
+		btn.classList.remove('press');
 	})
 }
