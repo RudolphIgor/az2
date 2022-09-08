8f28aa17f9b6bd2e446d84a98700affdddb3d0df
Rudolph <azazil47@gmail.com>
2022-09-08 17:18:55 +0700

    Чудо продолжается. Добавли вторйо слайд ничего не сломалось. Табы работают. Можно добавлять все галлереи

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,5 +1,5 @@
 const tabs = document.querySelector('.tabs__list');
-const gallSlider = document.querySelectorAll('.gallery__block');
+const gallSlider = document.querySelectorAll('.magicBlock');
 
 tabs.addEventListener('click', function (e) {
 	if (e.target.classList.contains('tabs__item')) {
