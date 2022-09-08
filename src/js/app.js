dfbc69535c5bb4430f27c931cda3aeea69410890
Rudolph <azazil47@gmail.com>
2022-09-08 10:24:24 +0700

    Реализовано переключение табов

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 import * as flsFunctions from "./modules/functions.js";
-import * as burger from "./modules/burger.js"
-import * as slowlyScroll from "./modules/slowly_scroll.js"
-import * as slider from "./modules/swiper.js"
-import * as btn_faq from "./modules/faq.js"
+import * as burger from "./modules/burger.js";
+import * as slowlyScroll from "./modules/slowly_scroll.js";
+import * as slider from "./modules/swiper.js";
+import * as btn_faq from "./modules/faq.js";
+import * as tabs from "./modules/tabs.js";
 
 flsFunctions.isWebp();
 slowlyScroll.slScroll();
 burger.iconsactive();
-btn_faq.swipe();
+// btn_faq.swipe();
